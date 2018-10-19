798fd8947335598c1637612c4a623fa49d251980
Cheryl Qian <cqian@stuy.edu>
2018-10-19 00:10:53 -0400

    updated devlog

--- db.py
+++ db.py
@@ -6,5 +6,24 @@ DB_FILE="discobandit.db"
 db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
 c = db.cursor()
 
-def getUser(name, word):
-    c.execute("CREATE TABLE IF NOT EXISTS usersDirectory(username TEXT, blogName TEXT, password TEXT, permissions TEXT)")
+c.execute("CREATE TABLE userDirectory(username TEXT, blogName TEXT, password TEXT, permissions TEXT)")
+    
+def getPw(user):
+    c.execute("SELECT password FROM userDirectory WHERE username = '{0}'".format(user))
+    return c.fetchone()
+
+def getBlog(user):
+    c.execute("SELECT blogName FROM userDirectory WHERE username = '{0}'".format(user))
+    return c.fetchone()
+
+def register(user, blog, pw, permission):
+    c.execute("INSERT INTO userDirectory VALUES('{0}','{1}', '{2}','{3}')".format(user,blog,pw,permission))
+
+register('Susan','sblog','pass','read')
+print getPw('Susan')
+print getBlog('Susan')
+
+
+        
+
+
