42430ca9891c089b7a4f4b570abae61955b4f4e3
Susan Lin <slin15@stuy.edu>
2018-10-21 16:47:14 -0400

    a

--- app.py
+++ app.py
@@ -5,13 +5,11 @@
 
 from flask import Flask, render_template, request, session, redirect, url_for, flash
 from os import urandom
-#import db
+import db
 
 app = Flask(__name__)
 
 app.secret_key = urandom(32)
-#hard coded valid username and password
-users = {"s": "1"}
 
 #home root
 @app.route('/', methods=["POST", "GET"])
@@ -24,18 +22,28 @@ def home():
 @app.route('/auth', methods=["POST", "GET"])
 def login():
     #username and passwords match
-    if request.args['user'] in users.keys() and users[request.args['user']] == request.args['pass']:
+    if db.isUser(request.args['user']) and db.getPw(request.args['user']) == request.args['pass']:
         return render_template('return.html', user=request.args['user'])
 
     #username doesn't match
-    elif request.args['user'] not in users.keys():
+    elif db.isUser(request.args['user']) == False:
         flash("username not found")
         return render_template('form.html', error=True)
 
     #password doesn't match username
-    elif users[request.args['user']] != request.args['pass']:
+    elif db.getPw(request.args['user']) != request.args['pass']:
         flash("password incorrect")
         return render_template('form.html', error=True)
+
+@app.route('/auth2', methods=["POST", "GET"])
+def makeAcc():
+    if db.isUser(request.args["Username"]):
+        flash("username not found")
+        return render_template('register.html', error=True)
+    elif db.isUser(request.args["Username"]) == False:
+        db.register(request.args['Username'], request.args['Blog'], request.args['Password'], "Read")
+        return redirect(url_for('home'))
+
 
 # Register route
 @app.route('/register', methods = ["POST", "GET"])
--- db.py
+++ db.py
@@ -2,46 +2,76 @@ import sqlite3   #enable control of an sqlite database
 import csv       #facilitates CSV I/O
 from datetime import datetime
 
-DB_FILE="discobandit.db"
+DB_FILE="gudetama.db"
 
-db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
-c = db.cursor()
+db = sqlite3.connect(DB_FILE, check_same_thread=False) #open if file exists, otherwise create
 
-c.execute("CREATE TABLE userDirectory(username TEXT, blogName TEXT, password TEXT, permissions TEXT)")
+def create():
+    c = db.cursor()
+    c.execute("CREATE TABLE IF NOT EXISTS userDirectory(username TEXT, blogName TEXT, password TEXT, permissions TEXT)")
+
+def isUser(user):
+    c = db.cursor()
+    c.execute("SELECT username FROM userDirectory WHERE username = '{0}'".format(user))
+    name = c.fetchone()[0]
+    if name != None and name == user:
+        return True
+    return False
+    db.commit()
+    db.close()
 
 def getPw(user):
+    c = db.cursor()
     c.execute("SELECT password FROM userDirectory WHERE username = '{0}'".format(user))
     return c.fetchone()
+    db.commit()
+    db.close()
+
 
 def resetPw(user, newPass):
+    c = db.cursor()
     c.execute("UPDATE userDirectory SET password = newPass WHERE username = '{0}'".format(user))
+    db.commit()
+    db.close()
 
 def getBlog(user):
+    c = db.cursor()
     c.execute("SELECT blogName FROM userDirectory WHERE username = '{0}'".format(user))
+    db.commit()
+    db.close()
     return c.fetchone()
 
 def getBlogTitle(user):
+    c = db.cursor()
     c.execute("SELECT title FROM '{0}' WHERE username = '{1}'".format(getBlog(user), user))
+    db.commit()
+    db.close()
     return c.fetchone()
 
 def getBlogBody(user):
+    c = db.cursor()
     c.execute("SELECT body FROM '{0}' WHERE username = '{1}'".format(getBlog(user), user))
+    db.commit()
+    db.close()
     return c.fetchone()
 
 def register(user, blog, pw, permission):
+    c = db.cursor()
     c.execute("INSERT INTO userDirectory VALUES('{0}','{1}', '{2}','{3}')".format(user,blog,pw,permission))
     # inputs username, blog name, password, and permission status into the user Directory database
-    c.execute("CREATE TABLE '{0}' (username TEXT, title TEXT, body TEXT, timeB TEXT)".format(blog))
+    c.execute("CREATE TABLE IF NOT EXISTS '{0}' (username TEXT, title TEXT, body TEXT, timeB TEXT)".format(blog))
     # creates the user's blog specific database which holds the date of entry, username, title of entry, and content
-    c.execute("CREATE TABLE '{0}' (username TEXT, editedVersion TEXT, title TEXT, timeB TEXT)".format(blog+"History"))
+    c.execute("CREATE TABLE IF NOT EXISTS '{0}' (username TEXT, editedVersion TEXT, title TEXT, timeB TEXT)".format(blog+"History"))
     # creates the user's blog edit history specific database which holds the username, content of edited version, title of entry, and date of edit
     c.execute("INSERT INTO '{0}' VALUES ('{1}', '{2}', '{3}', '{4}')".format(blog, user, "CREATED", "CREATED", datetime.utcnow()))
     # first entry of blogName: time is utc standard unless we have time to make user specifc; inserts all information given by user into fields
     c.execute("INSERT INTO '{0}' VALUES ('{1}', '{2}', '{3}', '{4}')".format(blog+"History", user, "CREATED", "CREATED", datetime.utcnow()))
     # first entry of blogNameHistory: time is is utc standard unless we have time to make user specifc; inserts all information given by user into fields
+    db.commit()
+    db.close()
 
-register('Susan','sblog','pass','read')
-print getPw('Susan')
-print getBlog('Susan')
-resetPw ('Susan', 'word')
-print getPw('Susan')
+#register('Susan','sblog','pass','read')
+##print getPw('Susan')
+#print getBlog('Susan')
+#resetPw ('Susan', 'word')
+#print getPw('Susan')
